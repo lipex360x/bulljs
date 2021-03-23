4cbee8fa3d42847b61ca06a920bb96f7cb304ff5
Felipe Borges <lipex360@gmail.com>
2021-03-22 22:10:35 -0300

    Route post user created

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import express from 'express'
+import UserController from './app/controllers/UsersControllers'
 
 const app = express()
+app.use(express.json())
+
+app.post('/users', UserController.store)
 
 app.listen(3333, () => {
   console.log('Server running on port 3333')
