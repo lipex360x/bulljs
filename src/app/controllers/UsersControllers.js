b28edef7a2d469883e2cf0f205912d788286f763
Felipe Borges <lipex360@gmail.com>
2021-03-22 22:22:14 -0300

    send mail ok

--- src/app/controllers/UsersControllers.js
+++ src/app/controllers/UsersControllers.js
@@ -1,3 +1,5 @@
+import Mail from '../lib/Mail'
+
 export default {
   async store(req, res) {
     const { name, email, password } = req.body
@@ -9,6 +11,12 @@ export default {
     }
 
     // Enviar email
+    await Mail.sendMail({
+      from: 'Queue Test <queue@queuetest.com>',
+      to: `${name} <${email}>`,
+      subject: 'Cadastro de Usuário',
+      html: `Olá, ${name}, bem-vindo ao sistema de filas`
+    })
 
     return res.json(user)
   }
